713d6de3cbecb37d03d9c36a2470f3e5979595a4
sabbir-anwar <protik26@gmail.com>
2019-01-15 15:06:50 +0600

    project initialize

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import{RouterModule, Routes} from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+const appRoutes:Routes=[
+  {
+    path:'',
+    component: LoginComponent
+  }
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
