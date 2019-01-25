29d836349068cdf2d0aace3262811cab6b2b5c16
ShovonSMZ <shovonmajumder99@gmail.com>
2019-01-25 19:11:09 +0600

    Form transition updated in Inventory item panel

--- src/app/inventory-items/item-form/item-form.component.ts
+++ src/app/inventory-items/item-form/item-form.component.ts
@@ -50,22 +50,30 @@ export class ItemFormComponent implements OnInit {
     this.init();
 
   }
+
+  lock = false;  
   submit(){
-    
+    this.lock = true;
+    if(this.item.name.length == 0||this.item.color_code.length == 0|| this.item.description.length==0) {
+      return;
+    }
+
     console.log(this.item);  
     let token=localStorage.getItem("token");
     let headers= new HttpHeaders().append("Authorization","Bearer "+token);
     this.http.post(getHost()+"/api/items",this.item,{headers}).subscribe((res)=>{
-       this.init();
+      this.sendMessageToParent(this);
+      this.init();
     },(error)=>{
       if(error.status ==201)
       {
+        this.sendMessageToParent(this);
          this.init();  
       }
     });
   }
   
-   sendMessageToParent(message:boolean)  {
+   sendMessageToParent(message:any)  {
     this.messageEvent.emit(message);
   }
   cancel()
@@ -73,12 +81,19 @@ export class ItemFormComponent implements OnInit {
     this.sendMessageToParent(false);
   }
   init(){
+    this.item={
+      name:"",
+      description:"",
+      color_code:"",
+      category_id:""
+  
+    }
    let token=localStorage.getItem("token");
    let headers= new HttpHeaders().append("Authorization","Bearer "+token);
    this.http.get(getHost()+"/api/categories",{headers}).subscribe((res)=>{
       this.categories=res;
       console.log(this.categories);  
-       this.sendMessageToParent(true);
+      //  this.sendMessageToParent(true);
       
    },(err)=>{
     console.log(err);  
